90e40aeb91ce08e570951ee20e665a034b0551af
kauemirandaindominus7966-code <kauemirandaindominus7966@gmail.com>
2025-10-24 15:47:04 -0300

    create: aula x

--- aula001_sessao03.py
+++ aula001_sessao03.py
@@ -1,1 +1,7 @@
-AULA 1 do modulo 3
+valor1 = int(input('Digite um valor: '))
+valor2 = int(input('Digite outro valor: '))
+
+if valor1 > valor2:
+    print('O valor1 é maior que o valor2')
+elif valor1 < valor2:
+    print('O valor1 é menor que o valor2')
--- aula002_sessao03.py
+++ aula002_sessao03.py
@@ -1,1 +1,9 @@
-AULA 1 do modulo 3
+valor1 = int(input('Digite um valor: '))
+valor2 = int(input('Digite outro valor: '))
+
+if valor1 < valor2:
+    print(f'O valor1 {valor1} é menor que o valor2 {valor2}')
+elif valor1 > valor2:
+    print(f'O valor1 {valor1} é maior que o valor2 {valor2}')
+else:
+    print(f'O valor1 {valor1} é igual que o valor2 {valor2}')
